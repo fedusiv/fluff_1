15dfa7c5737393db1c9d8c17a8f446b054701fe1
ilya <fedusivilya@gmail.com>
2018-05-25 17:44:48 +0300

    end of week commit

--- client.py
+++ client.py
@@ -1,6 +1,12 @@
 class Client:
+
+    recived_data = bytes
+
     def __init__(self):
         print("created")
 
-    def test_write(self):
-        print("client")
+    def show_data(self):
+        print(self.recived_data)
+
+    def recieving_data(self, bytes):
+        print(bytes)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # 23 05 home
-
 import socket
 import sys
 from _thread import start_new_thread
@@ -7,44 +6,34 @@ from client import Client
 
 HOST = '' # all availabe interfaces
 PORT = 9999 # arbitrary non privileged port
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
 print("[-] Socket Created")
-
-# bind socket
 s.bind((HOST, PORT))
 print("[-] Socket Bound to port " + str(PORT))
-
 s.listen(10)
 print("Listening...")
 
-# The code below is what you're looking for ############
+def client_thread(conn, addr):
+        cl = Client()
+        while True:
+            try:
+                data = conn.recv(1024)
+                cl.recived_data = data
+                cl.recieving_data(data)
+                cl.show_data()
+                conn.send(b"i'm server")
+            except socket.error as e:
+                print(e)
+                print("close socket")
+                break
 
-def client_thread(conn):
-    conn.send(b"Welcome to the Server. Type messages and press enter to send.\n")
 
-    while True:
-        try:
-            data = conn.recv(1024)
-            print(data)
-            conn.sendall(b"recieved")
-        except socket.error as e:
-            print(e)
-            print("close socket")
-            break
-
-    conn.close()
-
-cl = Client()
-cl.test_write()
 
 while True:
     # blocking call, waits to accept a connection
     conn, addr = s.accept()
     print("[-] Connected to " + addr[0] + ":" + str(addr[1]))
 
-    start_new_thread(client_thread, (conn,))
+    start_new_thread(client_thread, (conn, addr))
 
 s.close()
