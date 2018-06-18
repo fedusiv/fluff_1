48da6f87af314e525d58996c87dd4b2eac84c945
ilya <fedusivilya@gmail.com>
2018-06-18 17:35:33 +0300

    add json communication/ starting db / commands_list

--- client.py
+++ client.py
@@ -1,19 +1,33 @@
 import json
+import sqlite3
+
 
 class Client:
-
+    # recieved data from tcp
     received_data = dict
+    # list of json packets for sending
+    sending_fifo = []
+
+
 
     def parse_command_login(self):
-        if self.received_data["login"] == "ilya" and self.received_data["password"] == "555" :
-            print("correct")
-
+        if self.received_data["login"] == "ilya" and self.received_data["password"] == "555":
+            msg = json.dumps(
+                {"command": "login",
+                 "result": "correct"}
+            )
+            self.sending_fifo.append(msg)
+            print("correct client login")
         else:
-            print("incorrect")
+            msg = json.dumps(
+                {"command": "login",
+                 "result": "incorrect"}
+            )
+            self.sending_fifo.append(msg)
+            print("incorrect client login")
 
     def parse_command_password(self):
         print(self.received_data)
-
 
 
     commands_dict = {
@@ -35,3 +49,9 @@ class Client:
         except KeyError as e:
             print("got wrong command")
 
+
+    def sending_data(self):
+        if len(self.sending_fifo) > 0:
+            return self.sending_fifo.pop(0)
+        else:
+            return 0
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import socket
 import sys
 from _thread import start_new_thread
 from client import Client
+import sqlite3
 
 HOST = '' # all availabe interfaces
 PORT = 9999 # arbitrary non privileged port
@@ -13,9 +14,28 @@ print("[-] Socket Bound to port " + str(PORT))
 s.listen(10)
 print("Listening...")
 
+# connection with db
+sql_conn = sqlite3.connect("fluffdb.db")
+# cursor on db
+sql_cursor = sql_conn.cursor()
+
+sql_cursor.execute("""CREATE TABLE users
+                  (email text, login text, password text,
+                   money text)""")
+sql_cursor.execute("""INSERT INTO users
+    VALUES ('sentipon.n@gmail.com','ilya','555', '10')""")
+
+sql_conn.commit()
+
+
 def client_thread(conn, addr):
         cl = Client()
         while True:
+            # check are client got message for send
+            send_message = cl.sending_data()
+            if send_message != 0:
+                conn.send(send_message.encode("utf-8"))
+                print("send: "+send_message)
             try:
                 data = conn.recv(1024)
                 cl.recieving_data(data)
@@ -30,7 +50,6 @@ while True:
     # blocking call, waits to accept a connection
     conn, addr = s.accept()
     print("[-] Connected to " + addr[0] + ":" + str(addr[1]))
-
     start_new_thread(client_thread, (conn, addr))
 
 s.close()
